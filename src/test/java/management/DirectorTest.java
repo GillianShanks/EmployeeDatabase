275f93a05ee487885dae63c02fbb7feee5b2aa33
Gillian Shanks <GillianShanks88@gmail.com>
2019-09-30 17:40:53 +0100

    adds Director class, passes tests for all inherited Manager properties and budget getter

---
+++ src/test/java/management/DirectorTest.java
@@ -0,0 +1,53 @@
+package management;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class DirectorTest {
+
+    Director director;
+
+    @Before
+    public void before(){
+        director = new Director("Cole", "SN9735R", 35000, "Testing", 10000);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Cole", director.getName());
+    }
+
+    @Test
+    public void hasNINumber() {
+        assertEquals("SN9735R", director.getNiNumber());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(35000, director.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        director.raiseSalary(5000);
+        assertEquals(40000, director.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canGetPayBonus() {
+        assertEquals(350, director.payBonus(), 0.01);
+    }
+
+    @Test
+    public void hasDepartmentName() {
+        assertEquals("Testing", director.getDeptName());
+    }
+
+    @Test
+    public void hasBudget(){
+        assertEquals(10000, director.getBudget(), 0.01);
+    }
+}
